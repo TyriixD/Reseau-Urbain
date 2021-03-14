849f2afc9b7e17bd39c53b6e36cb0f0786553bf8
Tyrii <mayon_26@hotmail.fr>
2021-03-14 14:13:04 -0400

    Initial commit

---
+++ Graphe.cpp
@@ -0,0 +1,19 @@
+/**
+ * \file Graphe.cpp
+ * \brief Implémentation d'un graphe orienté.
+ * \author IFT-2008 & ?
+ * \version 0.1
+ * \date mars 2021
+ *
+ *  Travail pratique numéro 2
+ *
+ */
+
+#include "Graphe.h"
+//vous pouvez inclure d'autres librairies si c'est nécessaire
+
+namespace TP2
+{
+	//Mettez l'implémentation de vos méthodes ici.
+
+}//Fin du namespace
---
+++ ReseauInterurbain.cpp
@@ -0,0 +1,72 @@
+/**
+ * \file ReseauInterurbain.cpp
+ * \brief Implémentattion de la classe ReseauInterurbain.
+ * \author IFT-2008 & ?
+ * \version 0.1
+ * \date mars 2021
+ *
+ *  Travail pratique numéro 2
+ *
+ */
+#include <sstream>
+#include <fstream>
+#include "ReseauInterurbain.h"
+//vous pouvez inclure d'autres librairies si c'est nécessaire
+
+namespace TP2
+{
+
+// Méthode fournie
+void ReseauInterurbain::chargerReseau(std::ifstream & fichierEntree)
+{
+    if (!fichierEntree.is_open())
+        throw std::logic_error("ReseauInterurbain::chargerReseau: Le fichier n'est pas ouvert !");
+
+    std::string buff;
+
+	getline(fichierEntree, nomReseau);
+	nomReseau.erase(0, 20); // Enlève: Reseau Interurbain:
+
+	int nbVilles;
+
+	fichierEntree >> nbVilles;
+	getline(fichierEntree, buff); //villes
+
+	unReseau.resize(nbVilles);
+
+	getline(fichierEntree, buff); //Liste des villes
+
+	size_t i = 0;
+
+    getline(fichierEntree, buff); //Premiere ville
+
+    while(buff != "Liste des trajets:")
+    {
+        unReseau.nommer(i, buff);
+        getline(fichierEntree, buff);
+        i++;
+    }
+
+    while(!fichierEntree.eof())
+    {
+        getline(fichierEntree, buff);
+		std::string source = buff;
+		getline(fichierEntree, buff);
+		std::string destination = buff;
+
+		getline(fichierEntree, buff);
+		std::istringstream iss(buff);
+
+		float duree;
+		iss >> duree;
+
+		float cout;
+		iss >> cout;
+
+		unReseau.ajouterArc(unReseau.getNumeroSommet(source), unReseau.getNumeroSommet(destination), duree, cout);
+	}
+}
+
+//À compléter par l'implémentation des autres méthodes demandées
+
+}//Fin du namespace
